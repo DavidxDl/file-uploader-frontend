2118d560cc9d1555a5b40eba6c8e797b231cdab4
DavidxDl <david27.dev@gmail.com>
2025-07-15 19:07:07 -0400

    imrpoved header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import Modal from "./Modal";
 export default function Header() {
   return (
     <header className="p-2 bg-green-500 text-center shadow">
       <a href={"/"} className=" text-white text-xl font-bold ">
         Upload File
       </a>
+      <Modal />
     </header>
   );
 }
